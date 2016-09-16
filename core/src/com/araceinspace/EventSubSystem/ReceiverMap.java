de55c3a017c01677afa92c5f28bda5a000cbed8a
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-16 13:10:44 -0700

    Finished ReceiverMap.java
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/araceinspace/EventSubSystem/ReceiverMap.java
+++ core/src/com/araceinspace/EventSubSystem/ReceiverMap.java
@@ -1,6 +1,7 @@
 package com.araceinspace.EventSubSystem;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Isaac Assegai on 9/16/16.
@@ -11,4 +12,37 @@ import java.util.HashMap;
  * Event.TYPE.
  */
 public class ReceiverMap extends HashMap<Event.TYPE, List<EventReceiver>>{
+
+/* Field Variables */
+
+/* Constructors */
+
+    public ReceiverMap(){
+        super();
+    }
+/* Private Methods */
+
+/* Public  Methods */
+
+    /**
+     * Puts a designated EventReceiver into the
+     * appropriate list in the super Hashmap.
+     * @param type The Type of event we will reference this EventReceiver with.
+     * @param item The EventReceiver we are mapping to the type.
+     */
+    public void put(Event.TYPE type, EventReceiver item){
+        List l = super.get(type);
+        l.add(item);
+        super.put(type, l);
+    }
+
+    /**
+     * Returns a list of EventReceiver
+     * that are referenced by a certain type.
+     * @param type The type of EventReceiver we are looking for.
+     * @return A List of EventReceivers that want to be notified of this type.
+     */
+    public List<EventReceiver> get(Event.TYPE type){
+        return super.get(type);
+    }
 }
