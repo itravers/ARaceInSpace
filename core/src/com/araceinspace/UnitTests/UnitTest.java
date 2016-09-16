761e919f29d4d635b7353f208cf8a68ef812e69e
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-15 22:58:21 -0700

    Added UnitTest SubSystem
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ core/src/com/araceinspace/UnitTests/UnitTest.java
@@ -0,0 +1,9 @@
+package com.araceinspace.UnitTests;
+
+/**
+ * Created by Isaac Assegai on 9/15/16.
+ * Any unit tests will need to implement this interface.
+ */
+public interface UnitTest {
+    public boolean test();
+}
---
+++ core/src/com/araceinspace/UnitTests/UnitTester.java
@@ -0,0 +1,25 @@
+package com.araceinspace.UnitTests;
+
+/**
+ * Created by Isaac Assegai on 9/15/16.
+ * The UnitTests package is for development purposes only.
+ * None of these classes should end up being included in any
+ * other SubSystems.
+ *
+ * As classes in the game are developed, new UnitTests will be
+ * developed to make sure they do not break with later development.
+ */
+public class UnitTester {
+
+    /**
+     * Entry point for the UnitTester to start.
+     * @param args Not doing anything with these yet.
+     */
+    public static void main(String[] args){
+        new UnitTester();
+    }
+
+    public UnitTester(){
+
+    }
+}
