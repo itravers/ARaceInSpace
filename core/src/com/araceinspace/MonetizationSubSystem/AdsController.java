66f51604104b85eb48c8c76607686d53ff0bc21e
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-17 20:25:35 -0700

    Added AdsController interface.
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ core/src/com/araceinspace/MonetizationSubSystem/AdsController.java
@@ -0,0 +1,8 @@
+package com.araceinspace.MonetizationSubSystem;
+
+/**
+ * Created by Isaac Assegai on 9/17/16.
+ * The AdsController is an interface implemented
+ */
+public interface AdsController {
+}
