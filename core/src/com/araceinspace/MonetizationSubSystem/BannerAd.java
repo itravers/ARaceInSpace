a64015a1a0d35437fc85c68b96b552ddfec27648
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-22 14:22:59 -0700

    Working on implementing a basic GameAd.java class.
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/araceinspace/MonetizationSubSystem/BannerAd.java
+++ core/src/com/araceinspace/MonetizationSubSystem/BannerAd.java
@@ -1,21 +1,31 @@
 package com.araceinspace.MonetizationSubSystem;
 
+import com.badlogic.gdx.ApplicationAdapter;
+
 /**
  * Created by Isaac Assegai on 9/22/16.
+ * The BannerAd extends the base GameAd
+ * and provides all the functionality needed to load
+ * and show itself, provided a given
+ * libgdx Application Adapter.
  */
 public class BannerAd extends GameAd{
-    @Override
-    public String getID() {
 
+/* Field Variables */
+
+/* Constructors */
+
+    /**
+     * Create a new BannerAd with a specific ID and ApplicationAdapter.
+     *
+     * @param ID  The ID - Ususally manually obtained from ads service.
+     * @param app The App we are displaying the ads in.
+     */
+    public BannerAd(String ID, ApplicationAdapter app) {
+        super(ID, app);
     }
 
-    @Override
-    public boolean isLoaded() {
-        return false;
-    }
+/* Private Methods */
 
-    @Override
-    public boolean isShowing() {
-        return false;
-    }
+/* Public Methods */
 }
--- core/src/com/araceinspace/MonetizationSubSystem/GameAd.java
+++ core/src/com/araceinspace/MonetizationSubSystem/GameAd.java
@@ -7,12 +7,75 @@ import com.badlogic.gdx.ApplicationAdapter;
  * A GameAd is the base ad type.
  */
 public abstract class GameAd {
+/* Field Variables */
+
+    /**
+     * This ID is determined by the underlying
+     * ads system. Google Admob will give
+     * this ID when setting up an Admob Account.
+     */
     private String ID;
+
+    /**
+     * Allows us to keep track if an ad
+     * is currently loaded and ready to be shown.
+     */
     private boolean loaded;
+
+    /**
+     * Allows us to keep track if an ad
+     * is currently showing.
+     */
     private boolean showing;
+
+    /**
+     * The LibGdx ApplicationAdapter we are currently using
+     * this will be a AndroidApplication if we are on android
+     * I believe it will be a LWJGL container on desktop.
+     * It is used to access the apps views, to allow the
+     * ads to be shown.
+     */
     private ApplicationAdapter app;
 
-    public abstract String getID();
-    public abstract boolean isLoaded();
-    public abstract boolean isShowing();
+/* Constructors */
+
+    /**
+     * Create a new GameAd with a specific ID and ApplicationAdapter.
+     * @param ID The ID - Ususally manually obtained from ads service.
+     * @param app The App we are displaying the ads in.
+     */
+    public GameAd(String ID, ApplicationAdapter app){
+        this.ID = ID;
+        this.app = app;
+    }
+
+/* Private Methods */
+
+
+
+/* Public Methods */
+
+    /**
+     * Returns the Ad's Id to the caller
+     * @return The ID.
+     */
+    public String getID(){
+        return this.ID;
+    }
+
+    /**
+     * Lets the caller know if the Ad is loaded and ready to be shown.
+     * @return True if loaded.
+     */
+    public boolean isLoaded(){
+        return loaded;
+    }
+
+    /**
+     * Lets the caller know if the Ad is currently showing.
+     * @return True if currently showing.
+     */
+    public boolean isShowing(){
+        return showing;
+    }
 }
