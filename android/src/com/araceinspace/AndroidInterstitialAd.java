672b0e1e3deda8e1a682b45e491189bdc29f3026
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-23 13:45:39 -0700

    Added to interstitial code
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- android/src/com/araceinspace/AndroidInterstitialAd.java
+++ android/src/com/araceinspace/AndroidInterstitialAd.java
@@ -1,6 +1,12 @@
 package com.araceinspace;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.araceinspace.MonetizationSubSystem.GameAd;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.google.android.gms.ads.InterstitialAd;
 
 /**
  * Created by Isaac Assegai on 9/23/16.
@@ -14,6 +20,27 @@ public class AndroidInterstitialAd extends GameAd{
 
 /* Field Variables */
 
+    /**
+     * The interstitial ad that will be loaded from google.
+     */
+    private InterstitialAd interstitialAd;
+
+    /**
+     * The LibGdx ApplicationAdapter we are currently using
+     * this will be a AndroidApplication if we are on android
+     * I believe it will be a LWJGL container on desktop.
+     * It is used to access the apps views, to allow the
+     * ads to be shown.
+     */
+    private AndroidApplication app;
+
+    /**
+     * me is just an alternate reference to this, for
+     * use when code in a contained Runnable wants
+     * to refer to this.
+     */
+    private AndroidInterstitialAd me;
+
 
 /* Constructors */
 
@@ -21,9 +48,12 @@ public class AndroidInterstitialAd extends GameAd{
      * Create a new GameAd with a specific ID and ApplicationAdapter.
      *
      * @param ID The ID - Ususally manually obtained from ads service.
+     * @param app The App we are displaying the ads in.
      */
-    public AndroidInterstitialAd(String ID) {
+    public AndroidInterstitialAd(String ID, final AndroidApplication app) {
         super(ID);
+        this.app = app;
+        me = this;
     }
 
 
@@ -50,5 +80,17 @@ public class AndroidInterstitialAd extends GameAd{
     @Override
     public <T> void setVisibility(T vis) {
 
+    }
+
+    /**
+     * Check to see if the system's wifi is connected.
+     * @return True if wifi is connected.
+     */
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) app.getSystemService(Context.CONNECTIVITY_SERVICE);
+        //NetworkInfo ni = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+
+        return (ni != null && ni.isConnected());
     }
 }
