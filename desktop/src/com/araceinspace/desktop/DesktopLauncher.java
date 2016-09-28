5bb0fba13ad6b3be497ddbbe6cd8d5e386ae2de8
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-28 16:22:50 -0700

    Adding comments
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- desktop/src/com/araceinspace/desktop/DesktopLauncher.java
+++ desktop/src/com/araceinspace/desktop/DesktopLauncher.java
@@ -4,24 +4,24 @@ import com.araceinspace.MonetizationSubSystem.DummyController;
 import com.araceinspace.TestSubSystem.MonetizationIntegrationTest;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.araceinspace.ARaceInSpace;
 
+/**
+ * Created by Isaac Assegai on 9/17/16.
+ * The Desktop Launcher intializes all the
+ * systems to get the game working on the desktop.
+ */
 public class DesktopLauncher {
 
-	DummyController adsController;
+	DummyController monetizationController;
 	DesktopLauncher me;
-
-
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 800;
 		//config.useGL30 = true;
 		System.out.println("config h/w: " + config.height + "/" + config.width);
-		DummyController controller = new DummyController();
-
-
-
-		new LwjglApplication(new MonetizationIntegrationTest(controller, null), config);
+		DummyController monetizationController = new DummyController();
+		new LwjglApplication(new MonetizationIntegrationTest(monetizationController, null), config);
 	}
 }
