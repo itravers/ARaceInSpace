013c33bfad0c50278803897490997d375b09bba3
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-07-10 11:28:04 -0700

    Add GameObject.java in GameObjectSubSystem Package
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ core/src/com/araceinspace/GameObjectSubSystem/GameObject.java
@@ -0,0 +1,22 @@
+package com.araceinspace.GameObjectSubSystem;
+
+/**
+ * Created by Isaac Assegai on 7/10/17.
+ * This is the base abstract object class.
+ * All implemented game objects will be descendants of this class.
+ * To be a GameObject an object will need input, physics, graphics and state components.
+ * A game object will need update, create and dispose methods.
+
+ */
+public abstract class GameObject {
+    /* Field Variables & Objects */
+    InputComponent input;
+    PhysicsComponent physics;
+    GraphicsComponent graphics;
+    StateComponent state;
+
+    /* Abstract Methods */
+    abstract void update();
+    abstract void create();
+    abstract void dispose();
+}
