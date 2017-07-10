92d682d24f4ecbec766306c21dfd4d79bc02237f
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-07-10 12:04:00 -0700

    Add PlayerPhysicsComponent shell
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/araceinspace/GameObjectSubSystem/Components/PlayerPhysicsComponent.java
+++ core/src/com/araceinspace/GameObjectSubSystem/Components/PlayerPhysicsComponent.java
@@ -1,6 +1,9 @@
 package com.araceinspace.GameObjectSubSystem.Components;
 
 import com.araceinspace.GameObjectSubSystem.GameObject;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Isaac Assegai on 7/10/17.
@@ -12,13 +15,24 @@ import com.araceinspace.GameObjectSubSystem.GameObject;
 public class PlayerPhysicsComponent extends PhysicsComponent{
 
     /* Field Variables & Objects */
-    
+    World world;
+    Body body;
 
+    /**
+     * Create a new PlayerPhysicsComponent
+     */
+    public PlayerPhysicsComponent(){
+        //TODO Add constructor code.
+    }
 
     /* Methods */
 
+    /**
+     * Updates the Players Physics.
+     * @param o // The Player Object
+     */
     @Override
     void update(GameObject o) {
-
+        //TODO Add update code for player physics
     }
 }
