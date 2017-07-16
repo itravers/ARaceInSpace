40d79cd6d8bdb769e2e6f69e6831b1a6064631ac
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-07-16 13:34:47 -0700

    Remove Items from RenderManager that were already moved to individual screen
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/araceinspace/Managers/RenderManager.java
+++ core/src/com/araceinspace/Managers/RenderManager.java
@@ -1,6 +1,5 @@
 package com.araceinspace.Managers;
 
-import com.araceinspace.ARaceInSpace;
 import com.araceinspace.GameObjectSubSystem.Planet;
 import com.araceinspace.GameObjectSubSystem.Player;
 import com.araceinspace.GameWorld;
@@ -8,13 +7,7 @@ import com.araceinspace.Screens.INGAMEScreen;
 import com.araceinspace.Screens.TITLEScreen;
 import com.araceinspace.misc.OrthCamera;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.ArrayList;
 
@@ -30,19 +23,9 @@ public class RenderManager {
 
     /* Field Variables & Objects */
     public GameWorld parent;
-
-    private OrthCamera camera;
-    private OrthCamera backgroundCamera;
-    private Box2DDebugRenderer debugRenderer;
-    private Matrix4 debugMatrix;
-    private SpriteBatch batch;
-    private SpriteBatch backgroundBatch;
     private float baseZoom;
     private float cameraZoom;
 
-    private Viewport viewport;
-
-    private BitmapFont font;
     private TITLEScreen titleScreen;
     private INGAMEScreen ingameScreen;
 
@@ -62,25 +45,6 @@ public class RenderManager {
     private void setupRendering(){
         resetFrameNum();
         parent.animationManager.setupAnimations();
-        /*
-        font = new BitmapFont();
-        camera = new OrthCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        backgroundCamera = new OrthCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        batch = new SpriteBatch();
-        batch.setProjectionMatrix(camera.combined);
-
-        backgroundBatch = new SpriteBatch();
-        backgroundBatch.setProjectionMatrix(backgroundCamera.combined);
-
-        viewport = new ScreenViewport(camera);
-
-
-        debugRenderer = new Box2DDebugRenderer();
-        debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS, PIXELS_TO_METERS, 0);
-        *///moved to INGameScreen
-
-
         titleScreen = new TITLEScreen(this);
         ingameScreen = new INGAMEScreen(this);
         setupScreenSizeDependantItems();//must come after screens are constructed
@@ -102,58 +66,17 @@ public class RenderManager {
     }
 
 
-
     /**
      * Finds what level we are on and renders it
      * @param timeElapsed
      */
     private void renderInGame(float timeElapsed){
         ingameScreen.render(timeElapsed);
-
-        /*
-        Player p = parent.levelManager.getPlayer();
-        camera.zoom = cameraZoom;
-        backgroundCamera.zoom = cameraZoom;
-        camera.position.set(p.getX()+p.getWidth()/2, p.getY()+p.getHeight()/2, 0); //this causes a 2nd sprite to be printed???
-        camera.setToAngle(p.getPhysics().getBody().getAngle());
-        camera.update();
-
-        backgroundCamera.position.set(p.getX() + p.getWidth() / 2, p.getY() + p.getHeight() / 2, 0);
-        backgroundCamera.setToAngle(p.getPhysics().getBody().getAngle());
-        backgroundCamera.update();
-        backgroundBatch.setProjectionMatrix(backgroundCamera.combined);
-        batch.setProjectionMatrix(camera.combined);
-
-        //clear screen
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-        renderBackground(timeElapsed, backgroundBatch);
-
-
-
-        batch.begin();
-        renderPlayer(timeElapsed, batch);
-        renderPlanets(timeElapsed, batch);
-        if(parent.devMode){
-            renderVersion(batch);
-        }
-        batch.end();
-        //stage.act(timeElapsed);
-       // stage.draw();
-
-        debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS, PIXELS_TO_METERS, 0);
-       if(parent.devMode){
-           debugRenderer.render(parent.levelManager.getWorld(), debugMatrix);
-       }
-       */
     }
 
 
 
     public void renderBackground(OrthCamera backgroundCamera, float timeElapsed, SpriteBatch b){
-      //  System.out.println("RenderBackground");
         if(parent.levelManager.mainBackground == null)parent.levelManager.setupBackground();
         parent.levelManager.getBackground().render(backgroundCamera, timeElapsed, b);
     }
@@ -198,10 +121,6 @@ public class RenderManager {
 
     public void resetFrameNum(){
         setFrameNum(0);
-    }
-
-    public OrthCamera getBackgroundCamera(){
-        return backgroundCamera;
     }
 
     public float getCameraZoom() {
