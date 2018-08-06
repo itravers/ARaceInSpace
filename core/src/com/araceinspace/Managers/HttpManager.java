53f2d570dd0e83f730c5a80cae780dbcb2dcaba7
Isaac Assegai <Isaac.A.Travers@gmail.com>
2018-08-06 16:06:40 -0700

    Cleaned up httpManager.java
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/araceinspace/Managers/HttpManager.java
+++ core/src/com/araceinspace/Managers/HttpManager.java
@@ -4,10 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.net.HttpStatus;
-import com.badlogic.gdx.utils.ByteArray;
-import com.badlogic.gdx.utils.Json;
-import com.badlogic.gdx.utils.JsonWriter;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
@@ -16,30 +12,38 @@ import java.util.zip.ZipInputStream;
 
 /**
  * Created by Isaac Assegai on 7/31/17.
- * Class is to be used to make http post and get requests to scoreboard and ghost backend.
+ * Class is to be used to make http post and get requests to
+ * the leaderboard, ghost and level pack backend.
  */
 public class HttpManager {
-    boolean responseReady;
-    boolean requestFailed;
-    String responseJson;
-    byte [] responseBytes;
+    boolean responseReady; //Used by waitforresponse
+    boolean requestFailed; //Used to judge if the last request failed
+    String responseJson; //Used when we want to get JSON from the backend
+    byte [] responseBytes; //Used when we want to get pure data from the backend
 
     public HttpManager(){
         responseReady = false;
         requestFailed = false;
     }
 
+    /**
+     * Sending a request to the backend, expecting a string/json reponse
+     * @param url
+     * @param requestJson
+     * @param method
+     */
     public void sendRequest(String url, String requestJson, String method){
         sendRequest(url, requestJson, method, false);
     }
 
+    /**
+     * Sending a request to the backend
+     * @param url
+     * @param requestJson
+     * @param method
+     * @param getBytes //should this return data in responseBytes(true) or responseJSON(false)
+     */
     public  void sendRequest(String url, String requestJson, String method, final boolean getBytes) {
-        //System.out.println("httpManger: send request to: " + url);
-
-      //  final Json json = new Json();
-
-       // String requestJson = json.toJson(requestObject); // this is just an example
-
         Net.HttpRequest request = new Net.HttpRequest(method);
         request.setUrl(url);
 
@@ -47,14 +51,12 @@ public class HttpManager {
             request.setContent(requestJson);
         }
 
-
         request.setHeader("Content-Type", "application/json");
         request.setHeader("Accept", "application/json");
 
+        //Send the http request
         Gdx.net.sendHttpRequest(request, new Net.HttpResponseListener() {
-
             public void handleHttpResponse(Net.HttpResponse httpResponse) {
-
                 int statusCode = httpResponse.getStatus().getStatusCode();
                 if(statusCode != HttpStatus.SC_OK) {
                     System.out.println("Request Failed");
@@ -68,19 +70,11 @@ public class HttpManager {
                     responseJson = httpResponse.getResultAsString();
                 }
 
-
-
-
-
                 responseReady = true;
                 try {
-
-
                     //DO some stuff with the response string
-
                 }
                 catch(Exception exception) {
-
                     exception.printStackTrace();
                 }
             }
@@ -98,6 +92,10 @@ public class HttpManager {
         });
     }
 
+    /**
+     * Makes thread sleep until a reponse has come back from server
+     * @return
+     */
     public String waitForResponse(){
         while(!responseReady){
             if(requestFailed){
@@ -114,6 +112,11 @@ public class HttpManager {
         return responseJson;
     }
 
+    /**
+     * Makes thread sleep until a response has come back from server
+     * and we expecting pure data
+     * @return
+     */
     public byte[] waitForResponse_b(){
         while(!responseReady){
             if(requestFailed){
@@ -130,11 +133,14 @@ public class HttpManager {
         return responseBytes;
     }
 
+    /**
+     * Gets the fastest ghost from a specific challenge and level
+     * @param currentChallenge
+     * @param currentLevel
+     * @return
+     */
     public String readGhostFromServer(LevelManager.CHALLENGES currentChallenge, int currentLevel){
         String returnval = "";
-        //Gdx.net.openURI("192.168.1.197");
-        //String url = "http://192.168.1.197/html/araceinspace/";
-       // url += "level"+currentLevel + "-" + currentChallenge + "-ghost.json";
         int place = 0;
         switch(currentChallenge){
             case first:
@@ -158,6 +164,12 @@ public class HttpManager {
         return returnval;
     }
 
+    /**
+     * Gets a specific ghost from the server based on it's ghostID
+     * @param ghostID
+     * @param level
+     * @return
+     */
     public String readCustomGhostFromServer(String ghostID, int level){
         String returnval = "";
         String url = "http://192.168.1.197:3001/leaderboards/customghost/"+ghostID+"/"+level;
@@ -166,57 +178,76 @@ public class HttpManager {
         return returnval;
     }
 
+    /**
+     * Gets the leaderboards for a specific levelPack from the server
+     * @param levelPack
+     * @return
+     */
     public String readLeaderBoardFromServer(int levelPack){
         String returnval = "";
-        //Gdx.net.openURI("192.168.1.197");
         String url = "http://192.168.1.197:3001/leaderboards/json/"+levelPack;
-
         sendRequest(url, null, "GET");
         returnval = waitForResponse();
         return returnval;
     }
 
+    /**
+     * Submits a score to the server for a given level, place, with a name, and a time
+     * @param level
+     * @param place
+     * @param name
+     * @param time
+     * @return
+     */
     public String submitScore(int level, int place, String name, int time){
         String returnval = "";
-        //Gdx.net.openURI("192.168.1.197");
         String url = "http://192.168.1.197:3001/leaderboards/update/"+level+"/"+place+"/"+name+"/"+time;
-
         sendRequest(url, null, "GET");
         returnval = waitForResponse();
         return returnval;
     }
 
+    /**
+     * Submits a ghost replay to the server
+     * @param replay
+     * @param id
+     * @param level
+     * @param place
+     * @param name
+     * @param time
+     * @return
+     */
     public String submitGhostReplay(String replay, String id, int level, int place, String name, int time){
         System.out.println("Submit Ghost");
         String returnval = "";
-        //Gdx.net.openURI("192.168.1.197");
         String url = "http://192.168.1.197:3001/leaderboards/submitGhost/"+id+"/"+level+"/"+place+"/"+time+"/"+name;
-        //Json json = new Json(JsonWriter.OutputType.json);
-        //replay = json.toJson("test");
-
         sendRequest(url, replay, "POST");
         returnval = waitForResponse();
         return returnval;
     }
 
+    /**
+     * Returns the level leaders for a specific level pack
+     * @param levelPack
+     * @return
+     */
     public ArrayList<String> getLevelLeaders(int levelPack){
         ArrayList<String>returnVal = new ArrayList<String>();
         String url = "http://192.168.1.197:3001/leaderboards/levelleaders/"+levelPack;
         sendRequest(url, null, "GET");
         String leadersString = waitForResponse();
+
+        //check for errors, and return a correct data structure filled with error data
         if(leadersString == null || leadersString.contains("MongoError") || leadersString.contains("TypeError") || leadersString.contains("error no Level Pack")){
             for(int i = 0; i < 12; i++)returnVal.add("N/A");
             return returnVal;
         }
 
-        System.out.println("getLevelLeaders(): " + leadersString);
         StringTokenizer tokenizer = new StringTokenizer(leadersString, ":");
 
         while(tokenizer.hasMoreTokens()){
             returnVal.add(tokenizer.nextToken());
         }
-        //test Array
-       // for(int i = 0; i < 12; i++)returnVal.add("Slack");
         return returnVal;
     }
 
@@ -228,14 +259,9 @@ public class HttpManager {
     public boolean dlLevelPackFromServer(int levelPackToBuy){
         String url = "http://192.168.1.197:3001/levelpacks/"+levelPackToBuy;
         sendRequest(url, null, "GET", true);
-        //String s_levelPack = waitForResponse();
         byte[] levelPack = waitForResponse_b();
         if(false){
-           // for(int i = 0; i < 12; i++)returnVal.add("N/A");
-            //return returnVal;
-            //error here
         }else{
-            //byte[] levelPack = s_levelPack.getBytes();
             byte[] buffer = new byte[1024];
             if(levelPack == null)return false;//didn't work something is wrong
             ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(levelPack));
@@ -246,18 +272,12 @@ public class HttpManager {
                 while(zipEntry != null){
                     String fileName = zipEntry.getName();
                     System.out.println(System.getProperty("user.dir"));
-                    //File newFile = new File("unzipTest/" + fileName);
-                   // FileOutputStream fos = new FileOutputStream(newFile);
                     FileHandle file = Gdx.files.local("levels/"+levelPackToBuy+"/"+fileName);
 
-                   // file.writeString("My god, it's full of stars", false);
                     int len;
                     while ((len = zis.read(buffer)) > 0) {
-                       // fos.write(buffer, 0, len);
-                        //file.writeBytes(buffer, true);
                         file.writeBytes(buffer, 0, len, true);
                     }
-                    //fos.close();
 
                     zipEntry = zis.getNextEntry();
                 }
@@ -265,12 +285,6 @@ public class HttpManager {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-
-
-
-
         }
         return true;
     }
