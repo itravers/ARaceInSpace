8b48f9571df05d30531d7923b4e36ee55f9d44ed
Isaac Assegai <Isaac.A.Travers@gmail.com>
2018-08-11 17:41:14 -0700

    Added ConnectionManager
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ core/src/com/araceinspace/Managers/ConnectionManager.java
@@ -0,0 +1,12 @@
+package com.araceinspace.Managers;
+
+/**
+ *  Created by Isaac Assegai on 8/11/18
+ *  Connection Manager is the interface (not literally)
+ *  used to manage the connection to the backend server.
+ *  It asynchrously downloads, stores, and makes available
+ *  data from the backend
+ */
+public class ConnectionManager {
+    
+}
